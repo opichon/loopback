a6ad223e49ba46b87f94df669da4cdc8378a3bdf
Olivier Pichon <op@united-asian.com>
2017-07-04 21:24:57 +0700

    updated tests

--- test/test.js
+++ test/test.js
@@ -4,11 +4,20 @@ import {describe, it} from 'mocha'
 import supertest from 'supertest'
 import status from 'http-status'
 
-var api = supertest('http://127.0.0.1:3000')
+var api = supertest('http://localhost:3000')
 
 describe('Api is up and running', function () {
   it('should be able to open api url', function () {
     api.get('/')
+      .expect(status.OK, function (err) {
+        console.log(err)
+      })
+  })
+})
+
+describe('Api explorer is up and running', function () {
+  it('should be able to open api explorer url', function () {
+    api.get('/explorer')
       .expect(status.OK, function (err) {
         console.log(err)
       })
